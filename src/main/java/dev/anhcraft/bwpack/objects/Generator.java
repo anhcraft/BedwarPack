e76871a2ba508be4fa39b473d8dbaa824bb868c3
anhcraft <huynhduyanh123123@gmail.com>
2019-12-23 00:31:16 +0700

    Fixed distance measured error

--- src/main/java/dev/anhcraft/bwpack/objects/Generator.java
+++ src/main/java/dev/anhcraft/bwpack/objects/Generator.java
@@ -24,14 +24,16 @@ public class Generator extends ConfigurableObject {
     private String name;
 
     @Key("locations")
-    private List<Location> locations = new ArrayList<>();
+    private List<String> locations = new ArrayList<>();
 
     @Key("tiers")
     private Tier[] tiers = new Tier[0];
 
     @NotNull
     public List<Location> getLocations() {
-        return locations;
+        return locations.stream()
+                .map(LocationUtil::fromString)
+                .collect(Collectors.toList());
     }
 
     @NotNull
@@ -47,26 +49,19 @@ public class Generator extends ConfigurableObject {
     @Override
     protected @Nullable Object conf2schema(@Nullable Object value, ConfigSchema.Entry entry) {
         if(value != null){
-            switch (entry.getKey()){
-                case "locations": {
-                    return ((List<?>) value).stream()
-                            .map(s -> LocationUtil.fromString(s.toString()))
-                            .collect(Collectors.toList());
-                }
-                case "tiers": {
-                    ConfigurationSection cs = (ConfigurationSection) value;
-                    Set<String> x = cs.getKeys(false);
-                    Tier[] tiers = new Tier[x.size()];
-                    int i = 0;
-                    for(String s : x){
-                        try {
-                            tiers[i++] = ConfigHelper.readConfig(cs.getConfigurationSection(s), ConfigSchema.of(Tier.class));
-                        } catch (InvalidValueException e) {
-                            e.printStackTrace();
-                        }
+            if (entry.getKey().equals("tiers")) {
+                ConfigurationSection cs = (ConfigurationSection) value;
+                Set<String> x = cs.getKeys(false);
+                Tier[] tiers = new Tier[x.size()];
+                int i = 0;
+                for (String s : x) {
+                    try {
+                        tiers[i++] = ConfigHelper.readConfig(cs.getConfigurationSection(s), ConfigSchema.of(Tier.class));
+                    } catch (InvalidValueException e) {
+                        e.printStackTrace();
                     }
-                    return tiers;
                 }
+                return tiers;
             }
         }
         return value;
@@ -75,19 +70,14 @@ public class Generator extends ConfigurableObject {
     @Override
     protected @Nullable Object schema2conf(@Nullable Object value, ConfigSchema.Entry entry) {
         if(value != null){
-            switch (entry.getKey()) {
-                case "locations": {
-                    return locations.stream().map(LocationUtil::toString).collect(Collectors.toList());
+            if (entry.getKey().equals("tiers")) {
+                ConfigurationSection parent = new YamlConfiguration();
+                for (int i = 0; i < tiers.length; i++) {
+                    YamlConfiguration c = new YamlConfiguration();
+                    ConfigHelper.writeConfig(c, ConfigSchema.of(Tier.class), tiers[i]);
+                    parent.set(String.valueOf(i++), c);
                 }
-                case "tiers": {
-                    ConfigurationSection parent = new YamlConfiguration();
-                    for(int i = 0; i < tiers.length; i++){
-                        YamlConfiguration c = new YamlConfiguration();
-                        ConfigHelper.writeConfig(c, ConfigSchema.of(Tier.class), tiers[i]);
-                        parent.set(String.valueOf(i++), c);
-                    }
-                    return parent;
-                }
+                return parent;
             }
         }
         return value;
--- src/main/java/dev/anhcraft/bwpack/objects/Shopkeeper.java
+++ src/main/java/dev/anhcraft/bwpack/objects/Shopkeeper.java
@@ -2,12 +2,10 @@ package dev.anhcraft.bwpack.objects;
 
 import dev.anhcraft.battle.utils.ConfigurableObject;
 import dev.anhcraft.battle.utils.LocationUtil;
-import dev.anhcraft.confighelper.ConfigSchema;
 import dev.anhcraft.confighelper.annotation.*;
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,7 +23,7 @@ public class Shopkeeper extends ConfigurableObject {
     private String category;
 
     @Key("locations")
-    private List<Location> locations = new ArrayList<>();
+    private List<String> locations = new ArrayList<>();
 
     @NotNull
     public EntityType getEntityType() {
@@ -39,24 +37,8 @@ public class Shopkeeper extends ConfigurableObject {
 
     @NotNull
     public List<Location> getLocations() {
-        return locations;
-    }
-
-    @Override
-    protected @Nullable Object conf2schema(@Nullable Object value, ConfigSchema.Entry entry) {
-        if(value != null && entry.getKey().equals("locations")) {
-            return ((List<?>) value).stream()
-                    .map(s -> LocationUtil.fromString(s.toString()))
-                    .collect(Collectors.toList());
-        }
-        return value;
-    }
-
-    @Override
-    protected @Nullable Object schema2conf(@Nullable Object value, ConfigSchema.Entry entry) {
-        if(value != null && entry.getKey().equals("locations")) {
-            return locations.stream().map(LocationUtil::toString).collect(Collectors.toList());
-        }
-        return value;
+        return locations.stream()
+                .map(LocationUtil::fromString)
+                .collect(Collectors.toList());
     }
 }
