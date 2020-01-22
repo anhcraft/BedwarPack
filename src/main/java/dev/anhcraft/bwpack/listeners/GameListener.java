6d83f25662066ab73f5d69f4106ce081246906fa
anhcraft <huynhduyanh123123@gmail.com>
2020-01-22 11:29:51 +0700

    Fixed placed blocks not removed after game ends

--- src/main/java/dev/anhcraft/bwpack/listeners/GameListener.java
+++ src/main/java/dev/anhcraft/bwpack/listeners/GameListener.java
@@ -40,62 +40,67 @@ public class GameListener implements Listener {
 
     @EventHandler
     public void onPhaseUpdate(GamePhaseChangeEvent event){
-        if(event.getNewPhase() == GamePhase.PLAYING && event.getGame().getMode() == Mode.BEDWAR && event.getGame() instanceof LocalGame){
-            IBedWar bw = (IBedWar) Mode.BEDWAR.getController();
-            ExArena ea;
-            if(bw != null && (ea = bp.arenas.get(event.getGame().getArena().getId())) != null){
-                if(ea.getLocalGenerator() != null){
-                    TeamManager<BWTeam> x = bw.getTeamManager((LocalGame) event.getGame());
-                    if(x == null) return;
-                    List<Location> genLocs = new ArrayList<>(ea.getLocalGenerator().getLocations());
-                    List<BWTeam> bwts = new ArrayList<>(x.getTeams());
-                    outer:
-                    while (!genLocs.isEmpty()){
-                        for (Iterator<Location> it = genLocs.iterator(); it.hasNext(); ) {
-                            if(bwts.isEmpty()) break outer;
-                            Location genLoc = it.next();
-                            BWTeam chosenTeam = null;
-                            double nearestDist = 0;
-                            for(BWTeam bwt : bwts){
-                                double dist = genLoc.distanceSquared(bwt.getCenterSpawnPoint());
-                                if(chosenTeam != null && dist >= nearestDist){
-                                    continue;
+        if(event.getGame().getMode() == Mode.BEDWAR && event.getGame() instanceof LocalGame){
+            if(event.getNewPhase() == GamePhase.PLAYING) {
+                IBedWar bw = (IBedWar) Mode.BEDWAR.getController();
+                ExArena ea;
+                if (bw != null && (ea = bp.arenas.get(event.getGame().getArena().getId())) != null) {
+                    if (ea.getLocalGenerator() != null) {
+                        TeamManager<BWTeam> x = bw.getTeamManager((LocalGame) event.getGame());
+                        if (x == null) return;
+                        List<Location> genLocs = new ArrayList<>(ea.getLocalGenerator().getLocations());
+                        List<BWTeam> bwts = new ArrayList<>(x.getTeams());
+                        outer:
+                        while (!genLocs.isEmpty()) {
+                            for (Iterator<Location> it = genLocs.iterator(); it.hasNext(); ) {
+                                if (bwts.isEmpty()) break outer;
+                                Location genLoc = it.next();
+                                BWTeam chosenTeam = null;
+                                double nearestDist = 0;
+                                for (BWTeam bwt : bwts) {
+                                    double dist = genLoc.distanceSquared(bwt.getCenterSpawnPoint());
+                                    if (chosenTeam != null && dist >= nearestDist) {
+                                        continue;
+                                    }
+                                    chosenTeam = bwt;
+                                    nearestDist = dist;
                                 }
-                                chosenTeam = bwt;
-                                nearestDist = dist;
+                                bp.activeGenerators.put(event.getGame(), new ActiveGenerator(genLoc, ea.getLocalGenerator(), chosenTeam));
+                                bwts.remove(chosenTeam);
+                                it.remove();
                             }
-                            bp.activeGenerators.put(event.getGame(), new ActiveGenerator(genLoc, ea.getLocalGenerator(), chosenTeam));
-                            bwts.remove(chosenTeam);
-                            it.remove();
+                        }
+                    }
+                    for (Generator gen : ea.getSharedGenerators()) {
+                        for (Location loc : gen.getLocations()) {
+                            bp.activeGenerators.put(event.getGame(), new ActiveGenerator(loc, gen, null));
+                        }
+                    }
+                    Market mk = ApiProvider.consume().getMarket();
+                    for (Shopkeeper sk : ea.getShopkeepers()) {
+                        Optional<Category> ctg = mk.getCategories().stream().filter(c -> c.getId().equals(sk.getCategory())).findAny();
+                        if (!ctg.isPresent()) continue;
+                        Category ct = ctg.get();
+                        for (Location loc : sk.getLocations()) {
+                            Entity ent = loc.getWorld().spawnEntity(loc, sk.getEntityType());
+                            ent.setInvulnerable(true);
+                            ent.setMetadata("bpskp", new FixedMetadataValue(bp, ct));
+                            if (ct.getIcon().name() != null) {
+                                ent.setCustomNameVisible(true);
+                                ent.setCustomName(ChatUtil.formatColorCodes(ct.getIcon().name()));
+                            }
+                            if (ent instanceof LivingEntity) {
+                                LivingEntity x = (LivingEntity) ent;
+                                x.setAI(false);
+                                x.setCanPickupItems(false);
+                                x.setRemoveWhenFarAway(false);
+                            }
                         }
                     }
                 }
-                for(Generator gen : ea.getSharedGenerators()){
-                    for (Location loc : gen.getLocations()){
-                        bp.activeGenerators.put(event.getGame(), new ActiveGenerator(loc, gen, null));
-                    }
-                }
-                Market mk = ApiProvider.consume().getMarket();
-                for(Shopkeeper sk : ea.getShopkeepers()){
-                    Optional<Category> ctg = mk.getCategories().stream().filter(c -> c.getId().equals(sk.getCategory())).findAny();
-                    if(!ctg.isPresent()) continue;
-                    Category ct = ctg.get();
-                    for(Location loc : sk.getLocations()){
-                        Entity ent = loc.getWorld().spawnEntity(loc, sk.getEntityType());
-                        ent.setInvulnerable(true);
-                        ent.setMetadata("bpskp", new FixedMetadataValue(bp, ct));
-                        if(ct.getIcon().name() != null){
-                            ent.setCustomNameVisible(true);
-                            ent.setCustomName(ChatUtil.formatColorCodes(ct.getIcon().name()));
-                        }
-                        if(ent instanceof LivingEntity){
-                            LivingEntity x = (LivingEntity) ent;
-                            x.setAI(false);
-                            x.setCanPickupItems(false);
-                            x.setRemoveWhenFarAway(false);
-                        }
-                    }
-                }
+            } else if(event.getNewPhase() == GamePhase.END) {
+                //bp.activeGenerators.removeAll(event.getGame()); // this one is removed automatically
+                bp.placedBlocks.removeAll(event.getGame());
             }
         }
     }
